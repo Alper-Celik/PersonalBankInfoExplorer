6fc50c8c4f9cec68c20756bfacd4db27e72ac5ab
Alper Çelik <alper@alper-celik.dev>
2025-07-11 22:03:19 +0300

    add country seeding

--- DataOperator/Entities/Country.cs
+++ DataOperator/Entities/Country.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataOperator.Entities;
@@ -10,9 +11,17 @@ namespace DataOperator.Entities;
 [Table("Countries")]
 public class Country
 {
+  [JsonPropertyName("alpha3")]
   [Key]
   public required string Alpha3Code { get; set; }
+
+  [JsonPropertyName("alpha2")]
   public required string Alpha2Code { get; set; }
+
+  [JsonPropertyName("id")]
   public required short NumericCode { get; set; }
+
+  [JsonPropertyName("name")]
+  public required string EnglishName { get; set; }
 
 }
--- DataOperator/Entities/DataOperatorContext.cs
+++ DataOperator/Entities/DataOperatorContext.cs
@@ -1,9 +1,43 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 
 namespace DataOperator.Entities;
 
 public class DataOperatorContext : DbContext
 {
+
+  private readonly string? connectionString;
+
+  public DataOperatorContext(DbContextOptions<DataOperatorContext> options) : base(options) { }
+  public DataOperatorContext(string _connectionString)
+  {
+    this.connectionString = _connectionString;
+  }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    if (connectionString is not null)
+    {
+      optionsBuilder.UseSqlite(connectionString);
+    }
+    optionsBuilder.UseSeeding((context, _) => SeedCountries(context));
+  }
+
+  private static void SeedCountries(DbContext context)
+  {
+    var jsonPath = Path.Combine(Assembly.GetExecutingAssembly().Location, "SeedData", "countries.json");//from https://github.com/stefangabos/world_countries/blob/3480efd5b52aee45ebc22afa224cc05b70c500df/data/countries/en/countries.json
+    List<Country> countries = JsonSerializer.Deserialize<List<Country>>(File.ReadAllText(jsonPath)) ?? throw new UnreachableException("json not found");
+
+    foreach (var country in countries.Where(x => !context.Set<Country>().Contains(x)))
+    {
+      context.Set<Country>().Add(country);
+    }
+    context.SaveChanges();
+  }
 
   public DbSet<Country> Countries { get; set; }
   public DbSet<Currency> Currencies { get; set; }
