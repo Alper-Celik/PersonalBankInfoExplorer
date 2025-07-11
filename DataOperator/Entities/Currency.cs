e93ebea22c4374dd4ec6dc2524a10762fe025063
Alper Çelik <alper@alper-celik.dev>
2025-07-11 22:23:53 +0300

    add currency seeding

--- DataOperator/Entities/Currency.cs
+++ DataOperator/Entities/Currency.cs
@@ -1,18 +1,22 @@
 
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataOperator.Entities;
 
 [Index(nameof(CurrencyCode), IsUnique = true)]
-[Index(nameof(CurrencyNumber), IsUnique = true)]
 [Table("Currencies")]
 public class Currency
 {
+  [JsonPropertyName("code")]
   [Column(TypeName = ("char(3)")), Key]
   public required string CurrencyCode { get; set; }
-  public short? CurrencyNumber { get; set; }
+
+  [JsonPropertyName("name")]
   public string? Name { get; set; }
+
+  [JsonPropertyName("decimal_digits")]
   public required int MinorUnitFractions { get; set; } // see : https://en.wikipedia.org/wiki/ISO_4217#Minor_unit_fractions
 }
--- DataOperator/Entities/DataOperatorContext.cs
+++ DataOperator/Entities/DataOperatorContext.cs
@@ -24,7 +24,8 @@ public class DataOperatorContext : DbContext
     {
       optionsBuilder.UseSqlite(connectionString);
     }
-    optionsBuilder.UseSeeding((context, _) => SeedCountries(context));
+    optionsBuilder.UseSeeding((context, _) => SeedCountries(context))
+                  .UseSeeding((context, _) => SeedCurrincies(context));
   }
 
   private static void SeedCountries(DbContext context)
@@ -38,6 +39,27 @@ public class DataOperatorContext : DbContext
     }
     context.SaveChanges();
   }
+
+  private static void SeedCurrincies(DbContext context)
+  {
+    var jsonPath = Path.Combine(Assembly.GetExecutingAssembly().Location, "SeedData", "Common-Currency.json");//from gist.githubusercontent.com/ksafranski/2973986/raw/5fda5e87189b066e11c1bf80bbfbecb556cf2cc1/Common-Currency.json
+    var currencies_objects = JsonSerializer.Deserialize<JsonNode>(File.ReadAllText(jsonPath));
+
+    List<Currency> currencies = [];
+
+    foreach (var currency_obj in currencies_objects?.AsObject() ?? throw new UnreachableException("cant parse currency seed json"))
+    {
+      currencies.Add(JsonSerializer.Deserialize<Currency>(currency_obj.Value) ?? throw new UnreachableException("cannot parse currency seed json"));
+    }
+
+    foreach (var currency in currencies.Where(x => !context.Set<Currency>().Contains(x)))
+    {
+      context.Set<Currency>().Add(currency);
+    }
+    context.SaveChanges();
+  }
+
+
 
   public DbSet<Country> Countries { get; set; }
   public DbSet<Currency> Currencies { get; set; }
