96f1c07131e409be540e769ee0892096738ee81b
Alper Çelik <alper@alper-celik.dev>
2025-07-13 00:09:30 +0300

    add currency seeding test

--- BankDataDb.tests/BankDataContext_Tests.cs
+++ BankDataDb.tests/BankDataContext_Tests.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading.Tasks;
-using BankDataDb.Entities;
-using Microsoft.Data.Sqlite;
+﻿using BankDataDb.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace BankDataDb.tests;
@@ -22,5 +20,22 @@ public class BankDataContext_Tests
         Assert.Equal(alpha2, country.Alpha2Code);
         Assert.Equal(alpha3, country.Alpha3Code);
         Assert.Equal(name, country.EnglishName);
+    }
+
+    [Theory]
+    [InlineData("TRY", "Turkish Lira", 2)]
+    [InlineData("USD", "US Dollar", 2)]
+    [InlineData("EUR", "Euro", 2)]
+    [InlineData("JPY", "Japanese Yen", 0)]
+    public async Task BankDataContext_CurrencySeeding(string code, string name, byte MinorUnitFractions)
+    {
+        var context = new BankDataContext();
+
+        await context.Database.MigrateAsync();
+
+        Currency currency = await context.Currencies.Where(c => c.CurrencyCode == code).SingleAsync();
+        Assert.Equal(code, currency.CurrencyCode);
+        Assert.Equal(name, currency.Name);
+        Assert.Equal(MinorUnitFractions, currency.MinorUnitFractions);
     }
 }
