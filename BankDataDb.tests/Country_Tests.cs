8213daea76659ce273596fa2b080e12901c5ec22
Alper Çelik <alper@alper-celik.dev>
2025-07-13 19:17:02 +0300

    split BankDataContext_Tests to entity spesific tests

---
+++ BankDataDb.tests/Country_Tests.cs
@@ -0,0 +1,25 @@
+using BankDataDb.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankDataDb.tests;
+
+public class Country_Tests
+{
+
+  [Theory]
+  [InlineData("TR", "TUR", "Türkiye")]
+  [InlineData("GB", "GBR", "United Kingdom of Great Britain and Northern Ireland")]
+  [InlineData("DE", "DEU", "Germany")]
+  [InlineData("US", "USA", "United States of America")]
+  public async Task CountryConfigration_CountrySeeding(string alpha2, string alpha3, string name)
+  {
+    var context = new BankDataContext();
+
+    await context.Database.MigrateAsync();
+
+    Country country = await context.Countries.Where(c => c.Alpha3Code == alpha3).SingleAsync();
+    Assert.Equal(alpha2, country.Alpha2Code);
+    Assert.Equal(alpha3, country.Alpha3Code);
+    Assert.Equal(name, country.EnglishName);
+  }
+}
---
+++ BankDataDb.tests/Currency_Tests.cs
@@ -0,0 +1,24 @@
+using BankDataDb.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankDataDb.tests;
+
+public class Currency_Tests
+{
+  [Theory]
+  [InlineData("TRY", "Turkish Lira", 2)]
+  [InlineData("USD", "US Dollar", 2)]
+  [InlineData("EUR", "Euro", 2)]
+  [InlineData("JPY", "Japanese Yen", 0)]
+  public async Task CurrencyConfigration_CurrencySeeding(string code, string name, byte MinorUnitFractions)
+  {
+    var context = new BankDataContext();
+
+    await context.Database.MigrateAsync();
+
+    Currency currency = await context.Currencies.Where(c => c.CurrencyCode == code).SingleAsync();
+    Assert.Equal(code, currency.CurrencyCode);
+    Assert.Equal(name, currency.Name);
+    Assert.Equal(MinorUnitFractions, currency.MinorUnitFractions);
+  }
+}
