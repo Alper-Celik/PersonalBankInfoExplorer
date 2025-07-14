015eaee6387b64c975acaf2b143de1393794856c
Alper Çelik <alper@alper-celik.dev>
2025-07-14 23:21:13 +0300

    add index to Bank entity

--- BankDataDb/Entities/Bank.cs
+++ BankDataDb/Entities/Bank.cs
@@ -1,8 +1,11 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
+using Microsoft.EntityFrameworkCore;
+
 namespace BankDataDb.Entities;
 
 [Table("Banks")]
+[Index(nameof(Name), IsUnique = true)]
 public class Bank
 {
     public int Id { get; set; }
--- BankDataDb/Migrations/20250714200037_unique_index_to_Banks.Designer.cs
+++ BankDataDb/Migrations/20250714200037_unique_index_to_Banks.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BankDataDb.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BankDataDb.Migrations
 {
     [DbContext(typeof(BankDataContext))]
-    partial class BankDataContextModelSnapshot : ModelSnapshot
+    [Migration("20250714200037_unique_index_to_Banks")]
+    partial class unique_index_to_Banks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.7");
