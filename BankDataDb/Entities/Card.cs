a1583f42ea2f68ad2d9a61b22331812c6c6526d2
Alper Çelik <alper@alper-celik.dev>
2025-07-14 22:58:35 +0300

    remove unneccesary required keywords

--- BankDataDb/Entities/Card.cs
+++ BankDataDb/Entities/Card.cs
@@ -5,16 +5,16 @@ namespace BankDataDb.Entities;
 [Table("Cards")]
 public class Card
 {
-    public required int Id { get; set; }
+    public int Id { get; set; }
     public required string Name { get; set; }
 
     [Column(TypeName = "varchar(50)")]
     public CardTypes CardType { get; set; }
 
     [ForeignKey(nameof(Bank))]
-    public required int BankId { get; set; }
+    public int BankId { get; set; }
 
-    public required Bank IssuedBank { get; set; }
+    public Bank IssuedBank { get; set; } = null!;
 
 }
 
